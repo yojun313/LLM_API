307b5a2939852924fe9076ec2a3a8f1be0ebaba9
yojun313 <moonyojun@gmail.com>
2025-01-30 02:16:41 +0900

    Update Build_Chatbot.py

--- build/Build_Chatbot.py
+++ build/Build_Chatbot.py
@@ -24,6 +24,29 @@ def build_exe_from_spec(spec_file, output_directory):
     ])
     print(f"Finished building {exe_name}.exe")
 
+    # 삭제할 폴더 목록 정의
+    temp_folders = [
+        os.path.join(output_directory, 'build'),  # 빌드 폴더
+        os.path.join(os.path.dirname(spec_file), 'build'),  # 빌드 폴더 (추가 확인)
+        os.path.join(os.path.dirname(spec_file), 'dist'),  # PyInstaller dist 폴더
+        os.path.join(os.path.dirname(spec_file), '__pycache__'),  # __pycache__ 폴더
+    ]
+
+    # 삭제할 파일 목록 정의
+    temp_files = [
+        os.path.join(os.path.dirname(spec_file), f"{os.path.splitext(os.path.basename(spec_file))[0]}.spec")
+    ]
+
+    # 불필요한 폴더 삭제
+    for folder in temp_folders:
+        if os.path.exists(folder):
+            shutil.rmtree(folder)
+
+    # 불필요한 파일 삭제
+    for file in temp_files:
+        if os.path.exists(file):
+            os.remove(file)
+
 if __name__ == "__main__":
     if socket.gethostname() == "BigMacServer":
         output_directory = "C:/GitHub/BIGMACLAB/MANAGER/source"
